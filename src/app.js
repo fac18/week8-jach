6a6771298096dc06cc481e53af996fda307636dc
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2019-12-18 15:53:37 +0000

    connected model to controllers to app.js to index.js in route src folder

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const exphbs = require("express-handlebars");
 const path = require("path");
 
-const model = require("./model/index");
+const controller = require("./controllers/index");
 const helpers = require("./views/helpers/index");
 
 const app = express();
@@ -11,17 +11,17 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
 app.engine(
-    "hbs",
-    exphbs({
-        extname: "hbs",
-        layoutsDir: path.join(__dirname, "views", "layouts"),
-        partialsDir: path.join(__dirname, "views", "partials"),
-        defaultLayout: "main",
-        helpers
-    });
+  "hbs",
+  exphbs({
+    extname: "hbs",
+    layoutsDir: path.join(__dirname, "views", "layouts"),
+    partialsDir: path.join(__dirname, "views", "partials"),
+    defaultLayout: "main",
+    helpers
+  })
 );
 
 app.set("port", process.env.PORT || 2262);
-app.use(model);
+app.use(controller);
 
 module.exports = app;

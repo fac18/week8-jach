283c0fe29dee04a77dd7f693f5ec213c244def80
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-18 17:17:05 +0000

    still fixing postData
    Co-authored-by: pat-cki <pat.cichocki.uk@gmail.com>
    Co-authored-by: AlexandraOM <alexandra.omahony1@gmail.com>
    Co-authored-by: Ayub3 <a.mohamud1@hotmail.com>

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-const questionsList = require("./../model/index");
+const index = require("./../model/index");
+const questions = index.questions;
 
 router.get("/", (req, res) => {
   res.render("home", { questions: questionsList });
+});
+
+router.post("/create-user", (req, res) => {
+  res.render("home", {});
 });
 
 module.exports = router;

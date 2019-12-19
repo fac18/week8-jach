c2d122855ec928b00c54267117082fabd6b0276e
Roshan Thaabith <roshlarosh@gmail.com>
2019-12-19 14:20:42 +0000

    fix true or bad

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const behaviour = require("./behaviour");
 
 const index = require("./../model/index");
 const questionsList = index.questions;
@@ -13,8 +14,27 @@ console.log("I am here");
 
 router.post("/create-user", (req, res) => {
   console.log(req.body);
+  const questionsArray = [
+    req.body.q1,
+    req.body.q2,
+    req.body.q3,
+    req.body.q4,
+    req.body.q5,
+    req.body.q6,
+    req.body.q7
+  ];
+
+  let goodOrBad;
+  let totalPoint = behaviour(questionsArray);
+  if (totalPoint >= 4) {
+    goodOrBad = true;
+  } else {
+    goodOrBad = false;
+  }
+
   postData({
     name: req.body.username,
+    behaviour: goodOrBad,
     category: req.body.category
   });
 });
